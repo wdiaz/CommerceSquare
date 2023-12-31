90d07eaf91231b4b0eedad7bbd5340ee21b6259f
Walter <wdiaz503@gmail.com>
2023-12-30 21:44:03 -0500

    allowed endpoints to be public

--- src/main/java/com/commercesquare/api/security/SecurityConfig.java
+++ src/main/java/com/commercesquare/api/security/SecurityConfig.java
@@ -5,11 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +22,9 @@ public class SecurityConfig {
         return http.cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> {
-                    request.requestMatchers("/api/registration/register").permitAll();
-                    request.requestMatchers("swagger-ui/").permitAll();
+                    request.requestMatchers("api/**").permitAll();
+                    request.requestMatchers("swagger-ui/**").permitAll();
+                    request.requestMatchers("api-docs/**").permitAll();
                 }).build();
     }
 }
