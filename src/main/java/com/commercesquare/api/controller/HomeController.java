5b4eca77cc5a7adc326e625ec41568196cebe99e
Walter <wdiaz503@gmail.com>
2023-12-30 21:49:12 -0500

    added home controller

--- src/main/java/com/commercesquare/api/controller/HomeController.java
+++ src/main/java/com/commercesquare/api/controller/HomeController.java
@@ -1,8 +1,16 @@
 package com.commercesquare.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HomeController {
+
+    @GetMapping()
+    public String index() {
+        return "ok";
+    }
 
 }
